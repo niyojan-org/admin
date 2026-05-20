c39f07e7795334e6c91fbebe2650ed84a4bd3a5d
Abhishek Kumar <iamabhi0619@gmail.com>
2026-05-20 21:20:00 +0530

    Update auth redirect URL to orgatick.in

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -6,7 +6,7 @@ export default function AuthPage() {
   useEffect(() => {
     const origin = window.location.origin;
     //TODO: Change this to the actual login URL of your authentication service
-    const target = `https://iamabhi.me/login?redirect=${encodeURIComponent(origin)}`;
+    const target = `https://orgatick.in/login?redirect=${encodeURIComponent(origin)}`;
     window.location.replace(target);
   }, []);
 
